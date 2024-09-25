36120c546feb079a2fe4af3b998595f621187e8b
Neo <xuanquang.jrt@gmail.com>
2024-09-25 08:44:45 +0700

    testing

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shrimp',
+    version='1.0',
+    author='Neo',
+    description='A lightweight datastore handler.',
+    packages=find_packages(where='src'),
+    package_dir={'':'src'},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'],
+    python_requires='>=3.6'
+)
